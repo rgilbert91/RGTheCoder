f47a1130e1b59679f50c158d0ba1143b6dcebe26
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2020-01-28 15:19:46 -0500

    home pagewrap-up

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 export default function Home() {
     return (
-    <div>Home</div>);
+ <div className="header">   
+    <div className="welcome-text">
+        <h1>Hi, my name is Rasheem.</h1>
+        <h4>Work with me one on to make your website dreams come true.</h4>
+    </div>    
+    <div className="learn">
+      <Link to="/about">Learn More</Link>
+    </div>
+    
+  </div>);
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import logo from "../pix/rgTheCoder.png"
 
 export default function Navbar() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand logo d-inline-block align-top" href="/" >
+          <img src={logo} alt=""/>
+        </a>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+        <Link className="nav-link" to="/">Home</Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/about">About</Link>

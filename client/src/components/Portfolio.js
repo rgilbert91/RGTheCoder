f2d932dd7329e8d06fd72a55598ef25ac1c750b1
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2020-02-06 11:29:16 -0500

    final project touches

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,61 +1,116 @@
-import React from 'react';
-import picture from '../pix/brAInIAc.jpg'
-import ai from '../pix/AI.jpg'
+import React from "react";
+import picture from "../pix/brAInIAc.jpg";
+import ai from "../pix/AI.jpg";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio">
-<div className="container">
-  <div className="row justify-content-center">
-  <div className="col-md-m4">
-      <div className="card shadow" styles={{width: "18rem"}}>
-         <div className="inner">
-          <img className="card-img-top" 
-          src={picture} 
-          alt=""/>
+  return (
+    <div className="portfolio">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={picture} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">WebMaker</h5>
+                <p className="card-text">
+                  Project composing of both front end and back end work also
+                  added Bcrypt
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="card-body text-center">
-            <h5 className="card-title">WebMaker</h5>
-            <p className="card-text">
-            Project composing of both front end 
-            and back end work also added Bcrypt</p>
-            <a href="#" className=" btn btn-dark">See This Code</a>
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={ai} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">To-Do List</h5>
+                <p className="card-text">
+                  Simple project where you can add cross out and/or delete goals
+                  off of your list.
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
           </div>
-      </div>
-  </div>
-  <div className="col-md-m4">
-    <div className="card shadow" styles={{width: "18rem"}}>
-      <div className="inner">
-        <img className="card-img-top" 
-        src={ai} 
-        alt=""/>
-      </div>
-      <div className="card-body text-center">
-        <h5 className="card-title">To-Do List</h5>
-        <p className="card-text">
-        Simple project where you can add cross out and/or 
-        delete goals off of your list.</p>
-        <a href="#" className=" btn btn-dark">See This Code</a>
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={ai} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">To-Do List</h5>
+                <p className="card-text">
+                  Simple project where you can add cross out and/or delete goals
+                  off of your list.
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={ai} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">To-Do List</h5>
+                <p className="card-text">
+                  Simple project where you can add cross out and/or delete goals
+                  off of your list.
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={ai} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">To-Do List</h5>
+                <p className="card-text">
+                  Simple project where you can add cross out and/or delete goals
+                  off of your list.
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-m4" id='card'>
+            <div className="card shadow" styles={{ width: "18rem" }}>
+              <div className="inner">
+                <img className="card-img-top" src={ai} alt="" />
+              </div>
+              <div className="card-body text-center">
+                <h5 className="card-title">To-Do List</h5>
+                <p className="card-text">
+                  Simple project where you can add cross out and/or delete goals
+                  off of your list.
+                </p>
+                <a href="#" className=" btn btn-dark">
+                  See This Code
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div className="col-md-m4">
-  <div className="card shadow" styles={{width: "18rem"}}>
-      <div className="inner">
-        <img className="card-img-top" 
-        src={ai} 
-        alt=""/>
-      </div>
-      <div className="card-body text-center">
-        <h5 className="card-title">To-Do List</h5>
-        <p className="card-text">
-        Simple project where you can add cross out and/or 
-        delete goals off of your list.</p>
-        <a href="#" className=" btn btn-dark">See This Code</a>
-      </div>
-    </div>
-  </div>
-  </div>  
-</div>
-</div>
-);}
+  );
+}
--- client/src/components/Thankyou.js
+++ client/src/components/Thankyou.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 export default function Thankyou() {
-    return (
-        <div><div className="thanks jumbotron jumbotron-flex">
-            <h1>THANK YOU!</h1>
-            <span><h2>Thank you for the message, I will get back to you shortly.</h2></span>
-        </div>
-        <div>
-            <Link className="btn btn-block" to="/">
-            Home
-            </Link>
-        </div>
-    </div>)
+  return (
+    <div className="thanksB">
+      <div className="thanks jumbotron jumbotron-flex">
+        <h1>THANK YOU!</h1>
+        <span>
+          <h2>Thank you for the message, I will get back to you shortly.</h2>
+        </span>
+      </div>
+      <div id='center-btn'>
+        <Button className="btn btn-sumbit btn-outline-dark home-font" to="/">
+          Home
+        </Button>
+      </div>
+    </div>
+  );
 }

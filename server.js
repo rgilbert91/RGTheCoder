6c91a6e851c1747552c5d564f55e327464641c1d
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2020-02-06 14:30:17 -0500

    fixed4

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use(express.json({ extended: false}));
 if(process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(_dirname, "client", 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, "client", 'build', 'index.html'));
     });
 }
 
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3100;git 
 
 app.listen(PORT, () =>{
     console.log(`Server started on port ${PORT}`);
